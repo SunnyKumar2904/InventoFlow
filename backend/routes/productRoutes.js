6aa2481a37edbd1bd84f13f82beb9ad48dad2065
SunnyKumar2904 <sunnychoudharyy01@gmail.com>
2025-07-03 16:52:38 +0530

    Add files via upload

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,17 @@
+
+import express from "express";
+import {
+  getAllProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+router.get("/", getAllProducts);
+router.post("/", addProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
